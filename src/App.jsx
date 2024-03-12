df092170c69bcc6cce4963410db039123d8aba55
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-03-12 17:30:58 +0100

    minify app

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
 import { AssetRecordType, Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
 export default function App() {
-  const handleMount = useCallback((app) => {
-    const assetId = AssetRecordType.createId();
-    const videoAsset = {
-      id: assetId,
-      typeName: "asset",
-      type: "video",
-      props: {
-        w: 640, // Set the width of the video
-        h: 360, // Set the height of the video
-        name: "bunny.mp4",
-        isAnimated: true,
-        mimeType: "video/mp4",
-        src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-      },
-      meta: {},
-    };
-
-    app.createAssets([videoAsset]);
-    app.createShapes([
-      {
-        type: "video",
-        x: 100,
-        y: 100,
-        props: {
-          w: 640,
-          h: 360,
-          assetId,
-        },
-      },
-    ]);
-  }, []);
-
   return (
     <div
       style={{
@@ -43,7 +11,7 @@ export default function App() {
         height: "1000px",
       }}
     >
-      <Tldraw onMount={handleMount} />
+      <Tldraw />
     </div>
   );
 }
