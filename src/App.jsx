de0e48ac5b7cdbd1958f2db9c6ff967200ee4281
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-06 11:56:48 +0200

    compact version

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,16 @@ import "tldraw/tldraw.css";
 
 export default function App() {
   const handleMount = (editor) => {
-    const arrowId = createShapeId("arrow");
-
     const shapes = [
-      {
-        id: createShapeId("rectangle1"),
-        type: "geo",
-        x: 22,
-        y: -47,
-        props: {
-          align: "middle",
-          color: "black",
-          dash: "draw",
-          fill: "none",
-          font: "draw",
-          geo: "rectangle",
-          w: 149,
-          h: 90,
-          labelColor: "black",
-          scale: 1,
-          size: "l",
-          text: "Color",
-        },
-      },
-      {
-        id: createShapeId("rectangle2"),
-        type: "geo",
-        x: 150,
-        y: 150,
-        props: { geo: "rectangle", w: 50, h: 50, color: "yellow" },
-      },
-      {
-        id: createShapeId("rectangle3"),
-        type: "geo",
-        x: 210,
-        y: 150,
-        props: { geo: "rectangle", w: 100, h: 75, color: "red" },
-      },
-      { id: arrowId, type: "arrow", x: 100, y: 100 },
+      { id: createShapeId("rectangle1"), type: "geo", x: 22, y: -47, props: { align: "middle", color: "black", dash: "draw", fill: "none", font: "draw", geo: "rectangle", w: 149, h: 90, labelColor: "black", scale: 1, size: "l", text: "Color" } },
+      { id: createShapeId("rectangle2"), type: "geo", x: 150, y: 150, props: { geo: "rectangle", w: 50, h: 50, color: "yellow" } },
+      { id: createShapeId("rectangle3"), type: "geo", x: 210, y: 150, props: { geo: "rectangle", w: 100, h: 75, color: "red" } },
+      { id: createShapeId("arrow"), type: "arrow", x: 100, y: 100 }
     ];
 
     const bindings = [
-      {
-        fromId: arrowId,
-        toId: shapes[0].id,
-        type: "arrow",
-        props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 } },
-      },
-      {
-        fromId: arrowId,
-        toId: shapes[1].id,
-        type: "arrow",
-        props: { terminal: "end", normalizedAnchor: { x: 0.5, y: 0.5 } },
-      },
+      { fromId: shapes[3].id, toId: shapes[0].id, type: "arrow", props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 } } },
+      { fromId: shapes[3].id, toId: shapes[1].id, type: "arrow", props: { terminal: "end", normalizedAnchor: { x: 0.5, y: 0.5 } } }
     ];
 
     editor.createShapes(shapes);
@@ -63,17 +20,8 @@ export default function App() {
   };
 
   return (
-    <>
-      <div
-        style={{
-          position: "absolute",
-          width: 500,
-          height: 300,
-          top: "50px",
-        }}
-      >
-        <Tldraw onMount={handleMount}></Tldraw>
-      </div>
-    </>
+    <div style={{ position: "absolute", width: 500, height: 300, top: "50px" }}>
+      <Tldraw onMount={handleMount} />
+    </div>
   );
 }
