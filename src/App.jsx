8472971a1a740ea8e4e0d257235167ee0175529d
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-06 11:53:33 +0200

    add shapes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
 import { Tldraw, createShapeId } from "tldraw";
 import "tldraw/tldraw.css";
 
 export default function App() {
-
   const handleMount = (editor) => {
-    editor.createShapes([
-      {
-        id: createShapeId(),
-        type: "geo",
-        x: 300,
-        y: 300,
-        props: {
-          geo: "rectangle",
-          w: 150,
-          h: 75,
-          color: "blue",
-        },
-      },
-    ]);
+    const arrowId = createShapeId("arrow");
+
+    const shapes = [
+      { id: createShapeId("rectangle1"), type: "geo", x: 50, y: 50, props: { geo: "rectangle", w: 75, h: 37.5, color: "blue" } },
+      { id: createShapeId("rectangle2"), type: "geo", x: 150, y: 150, props: { geo: "rectangle", w: 50, h: 50, color: "yellow" } },
+      { id: createShapeId("rectangle3"), type: "geo", x: 210, y: 150, props: { geo: "rectangle", w: 100, h: 75, color: "red" } },
+      { id: arrowId, type: "arrow", x: 100, y: 100 },
+    ];
+
+    const bindings = [
+      { fromId: arrowId, toId: shapes[0].id, type: "arrow", props: { terminal: "start", normalizedAnchor: { x: 0.5, y: 0.5 } } },
+      { fromId: arrowId, toId: shapes[1].id, type: "arrow", props: { terminal: "end", normalizedAnchor: { x: 0.5, y: 0.5 } } },
+    ];
+
+    editor.createShapes(shapes);
+    editor.createBindings(bindings);
   };
 
   return (
@@ -26,8 +26,8 @@ export default function App() {
       <div
         style={{
           position: "absolute",
-          width: "1000px",
-          height: "1000px",
+          width: 500,
+          height: 500,
           top: "50px",
         }}
       >
