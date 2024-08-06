fffa8514fe919e87b8b712703f920af4ddfed268
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-06 11:59:57 +0200

    next one

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   const handleMount = (editor) => {
     const shapes = [
       { id: createShapeId("rectangle1"), type: "geo", x: 22, y: -47, props: { align: "middle", color: "black", dash: "draw", fill: "none", font: "draw", geo: "rectangle", w: 149, h: 90, labelColor: "black", scale: 1, size: "l", text: "Color" } },
-      { id: createShapeId("rectangle2"), type: "geo", x: 150, y: 150, props: { geo: "rectangle", w: 50, h: 50, color: "yellow" } },
+      { id: createShapeId("rectangle2"), type: "geo", x: 211, y: 88, props: { align: "middle", color: "orange", dash: "draw", fill: "solid", font: "draw", geo: "rectangle", growY: 0, h: 136, labelColor: "black", scale: 1, size: "xl", text: "", url: "", verticalAlign: "middle", w: 114 } },
       { id: createShapeId("rectangle3"), type: "geo", x: 210, y: 150, props: { geo: "rectangle", w: 100, h: 75, color: "red" } },
       { id: createShapeId("arrow"), type: "arrow", x: 100, y: 100 }
     ];
