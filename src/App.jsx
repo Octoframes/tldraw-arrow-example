ac29c6a20db273ba00fd5ee625df71c3d33b7eb2
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-09-13 11:52:24 +0200

    iris

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ export default function App() {
   const handleMount = (editor) => {
     const centerX = 300;
     const centerY = 200;
-    const petal_width = 10;
-    const petal_height = 20;
+    const petal_width = 40;
+    const petal_height = 100;
+    const sepal_width = 50;
+    const sepal_height = 110;
     const angles = [0, (Math.PI * 2) / 3, (Math.PI * 4) / 3];
+    const anglesBack = angles.map((angle) => angle + Math.PI / 3); // Adjusted angles
 
-    // Create shapes with adjusted positions and dimensions
     const shapes = [
-      // Background Ellipses (petals) with swapped width and height
+      // Back petals (light-violet, using sepal dimensions)
+      ...anglesBack.map((angle, index) => {
+        const id = createShapeId(`bgEllipseBack${index + 1}`);
+        const x = centerX + Math.sin(angle) * (sepal_width / 2);
+        const y = centerY - Math.cos(angle) * (sepal_width / 2);
+        return {id,type: "geo",x,y,rotation: angle,props: { h: sepal_width, w: sepal_height, color: "light-violet", dash: "draw", fill: "solid", font: "draw", geo: "ellipse", size: "xl" }};
+      }),
+      // Front petals (violet)
       ...angles.map((angle, index) => {
         const id = createShapeId(`bgEllipse${index + 1}`);
-        // Adjust position calculations to use the new dimensions
         const x = centerX + Math.sin(angle) * (petal_width / 2);
         const y = centerY - Math.cos(angle) * (petal_width / 2);
-        return {id,type: "geo",x,y,rotation: angle,props: { h: petal_width, w: petal_height, color: "violet", dash: "draw", fill: "solid", font: "draw", geo: "ellipse", size: "xl" },
-        };
+        return {id,type: "geo",x,y,rotation: angle,props: { h: petal_width, w: petal_height, color: "violet", dash: "draw", fill: "solid", font: "draw", geo: "ellipse", size: "xl" }};
       }),
       // Center Ellipse
-      {id: createShapeId("centerEllipse"),type: "geo",x: centerX - 2.5,y: centerY - 2.5,props: { h: 5, w: 5, color: "yellow", dash: "draw", fill: "solid", font: "draw", geo: "ellipse", size: "xl" },
-      },
+      {id: createShapeId("centerEllipse"),type: "geo",x: centerX - 25,y: centerY - 25,props: { h: 50, w: 50, color: "yellow", dash: "draw", fill: "solid", font: "draw", geo: "ellipse", size: "xl" }},
     ];
 
     // Create shapes
